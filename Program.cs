75e517a4bc5d73298aec980e92fdac7658bde0b0
Tomasz Pleśniak <toma1777@easv365.dk>
2020-01-27 11:53:59 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DBD_ass1_ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
